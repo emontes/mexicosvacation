d5fa224590e29cadda6356f313ac7f92609ec22c
emontes <emontes@turista.com.mx>
2020-06-28 23:34:46 -0500

    se agrega lo necesario para que funcione como PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,12 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
 module.exports = {
   siteMetadata: {
-    title:"Mexicos's Vacation",
-    description: "This is the place to Find the best deals for your Vacation in Mexico | HOTELS IN MEXICO | FLIGHTS TO MEXICO | CAR RENTALS",
+    title: "Mexicos's Vacation",
+    description:
+      "This is the place to Find the best deals for your Vacation in Mexico | HOTELS IN MEXICO | FLIGHTS TO MEXICO | CAR RENTALS",
     author: "@turistamexico",
-    twitterUsername:"@turistamexico",
-    image:'/mexicos-vacation.jpg',
-    siteUrl: "https://mexicosvacation.com"
+    twitterUsername: "@turistamexico",
+    image: "/mexicos-vacation.jpg",
+    siteUrl: "https://mexicosvacation.com",
   },
   plugins: [
     `gatsby-plugin-styled-components`,
@@ -18,7 +15,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-transition-link`,
     `gatsby-plugin-sitemap`,
-    'gatsby-plugin-robots-txt',
+    "gatsby-plugin-robots-txt",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -26,5 +23,18 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Mexico's Vacation`,
+        short_name: `Mex Vac`,
+        start_url: `/`,
+        background_color: `#effffd`,
+        theme_color: `#1BB297`,
+        display: `standalone`,
+        icon: `src/images/logo.jpg`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
